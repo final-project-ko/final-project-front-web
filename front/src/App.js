4da36ebb2570d91a19ec4f485385f349315e8223
OscarWilde <aksmfdyfl111@gmail.com>
2024-03-19 18:08:53 +0900

    mainScreen(HeaderNews.js) - 로딩화면 추가

--- front/src/App.js
+++ front/src/App.js
@@ -16,9 +16,6 @@ function App() {
     const { userId, setUserInfo } = useStore();
     const [auth,setAuth] = useState();
 
-   
-
-
 
   return (
     <>
--- front/src/page/news/HeaderNews.js
+++ front/src/page/news/HeaderNews.js
@@ -2,30 +2,29 @@ import { useNavigate, useParams } from "react-router-dom";
 import "../../components/css/HeaderNews.css";
 import DetailsNews from "./DetailsNews";
 import { useEffect, useState } from "react";
+import "../../components/css/Splash.css"; // 2024-03-19 스플래시(로딩)화면 css 추가
 
 const HeaderNews = () => {
-
-  // 파라미터 값을 url을 통해 넘겨받은 페이지에서 사용할 수 있도록 해줌.
-  let { category } = useParams();
-
-
-  if (category === undefined) {
-    category = "kr_total";
-  }
-  console.log(category);
-  // 카테고리 안에 기본은 main 나머지는 각 카테고리 이름을 담고 있음.. 그 카테고리 기사를 받아와 각 news에 뿌려주는 로직 작성 필요
-
-  const navigate = useNavigate();
-
-  /* 뉴스 클릭시 해당 뉴스 상세페이지 이동 */
-  const onClickHandler = (article) => {
-
-    navigate(`/detailNews/${article.code}`, { state: { article, articles } }); // 선택한 뉴스, 전체 뉴스 정보를 상세페이지에 보내줌
-  }
-
+  const [showSplash, setShowSplash] = useState(true); 
+  const [splashClass, setSplashClass] = useState("splash-screen");
+  const navigate = useNavigate(); // 이동
+  const { category: initialCategory } = useParams(); // 카테고리 가져오기
+  const [category, setCategory] = useState(initialCategory || "kr_total"); // 초기 카테고리 설정
   const [articles, setArticles] = useState([]);
 
-  /* 카테고리 별 뉴스 조회 API 요청 */
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSplashClass("splash-screen fade-out"); // fade-in 애니메이션 적용
+      setTimeout(() => {
+        setShowSplash(false); 
+      }, 700); 
+    }, 300); 
+  
+    return () => clearTimeout(timer);
+  }, []);
+
+
   useEffect(() => {
     const fetchCategoryNews = async () => {
       try {
@@ -40,11 +39,34 @@ const HeaderNews = () => {
       }
     };
     fetchCategoryNews();
-  }, [category]); // 카테고리가 변경될때마다 api요청
+  }, [category]); // 카테고리가 변경될 때마다 API 요청
+
+  if (showSplash) {
+    return (
+        <div style={{ 
+          backgroundColor: '#f7f9fb', 
+          width: '100vw', 
+          height: '100vh', 
+          position: 'fixed', // or 'absolute'
+          top: 0, 
+          left: 0 
+        }}>
+        <div className={splashClass} >
+          <img src="cup-lite.gif" alt="loading..." style={{ height: '10vh' }}></img>
+        </div>
+      </div>
+    );
+  }
+
+
+  /* 뉴스 클릭 시 해당 뉴스 상세페이지 이동 */
+  const onClickHandler = (article) => {
+    navigate(`/detailNews/${article.code}`, { state: { article, articles } }); // 선택한 뉴스, 전체 뉴스 정보를 상세페이지에 보내줌
+  }
 
 
   return (
-    /* api요청으로 받아 온 뉴스를 하나씩 담아준다 */
+    /* API 요청으로 받아온 뉴스를 하나씩 담아준다 */
     <div className='mainDiv'>
       {articles.map((article, index) => (
         <button className="new1" id={`news${index + 1}`} key={index} onClick={() => onClickHandler(article)}>
